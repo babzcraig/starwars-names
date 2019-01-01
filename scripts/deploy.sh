b82288bc9d13f341d9d1f0d4097f080ebe13bb02
Babs Craig <babscraig@gmail.com>
2019-01-01 15:07:17 +0100

    chore(config): Add babel support ot both src and test modules
    ES^ provides a wider range of features ans sybtaxt.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,8 @@
+npm run test
+# We start by checking coverage after travis runs our tests for us
 npm run check-coverage
+# We build so that we have a dist version of our package
+npm run build
+# Then we report the coverage and let semantic-release do it's thing
 npm run report-coverage
 npx semantic-release
